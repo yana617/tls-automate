56c4dfc0dd0021124e2d1dd8ab3a8856aa7d0cb8
Yana <jana.ru.sidorova@yandex.ru>
2023-06-15 15:02:18 +0300

    select date if availbale

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const audio = new Audio();
 audio.src = 'http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a';
 
 function makeAttention() {
-  document.title = "!! ВНИМАНИЕ !!";
+  document.title = "!! ДАТЫ !!";
   audio.play();
 
   setTimeout(() => makeAttention(), 800);
@@ -14,7 +14,7 @@ function getBlockElement() {
 
 const blockElement = getBlockElement();
 if (!blockElement) {
-  makeAttention();
+  document.title = "-- нет слотов";
 }
 
 if (!localStorage.getItem('savedBlock')) {
@@ -33,3 +33,28 @@ if (blockHtml !== savedHtml) {
   makeAttention();
   localStorage.setItem('savedBlock', blockElement?.outerHTML);
 }
+
+// -----
+
+const catchDateSlot = (classToClick) => {
+	const allGreenSlots = document.querySelectorAll(classToClick);
+	
+	console.log('allGreenSlots', allGreenSlots.length);
+	
+	if (allGreenSlots.length) {
+		let slotToClick;
+		
+		if (allGreenSlots.length > 2) {
+			slotToClick = allGreenSlots[2];
+		} else if (allGreenSlots.length > 1) {
+			slotToClick = allGreenSlots[1];
+		} else {
+			slotToClick = allGreenSlots[0];
+		}
+		
+		slotToClick.click();
+	}
+}
+
+const CLASS_TO_FIND = 'a.dispo'
+catchDateSlot(CLASS_TO_FIND);
