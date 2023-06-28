01000ccf7d3d1ab33b1db91386e7db90a3872bb1
Yana <jana.ru.sidorova@yandex.ru>
2023-06-28 15:41:29 +0300

    Add prime select

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-const audio = new Audio();
-audio.src = 'http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a';
-
-function makeAttention() {
-  document.title = "!! ДАТЫ !!";
-  audio.play();
-
-  setTimeout(() => makeAttention(), 800);
-}
-
 setTimeout(() => {
   window.scroll(0, 0);
   window.scrollBy(0, 130);
@@ -16,43 +6,107 @@ setTimeout(() => {
 const CLASS_TO_FIND = 'a.dispo';
 const allGreenSlots = document.querySelectorAll(CLASS_TO_FIND);
 
-function getRandomNumber(min, max) { // max excluded
-  return Math.floor(Math.random() * (max - min) + min);
-}
+const DATES = () => {
+  document.title = "!! ДАТЫ !!";
+  const audio = new Audio();
+  audio.src = 'http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a';
 
-const catchRandomDateSlot = () => {
-  const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
-  const slotToClick = allGreenSlots[randomSlotNumber];
-  slotToClick.click();
-}
-
-const catchNoPrimeDateSlot = (clickPrimeIfOnlyPrimeIsLeft = false) => {
-  const primeTime = ['15:30', '16:00'];
-  const isOnlyPrimeLeft =  [...allGreenSlots].every((slot) => primeTime.includes(slot.innerHTML));
-
-  if (isOnlyPrimeLeft) {
-    if (clickPrimeIfOnlyPrimeIsLeft) {
-      catchRandomDateSlot();
-    }
-    return;
+  function getRandomNumber(min, max) { // max excluded
+    return Math.floor(Math.random() * (max - min) + min);
   }
 
-  const clickNoPrimeDate = () => {
+  const catchRandomDateSlot = () => {
     const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
     const slotToClick = allGreenSlots[randomSlotNumber];
-    if (primeTime.includes(slotToClick.innerHTML)) {
-      return clickNoPrimeDate();
-    }
     slotToClick.click();
   }
 
-  clickNoPrimeDate();
-}
+  const catchNoPrimeDateSlot = (clickPrimeIfOnlyPrimeIsLeft = false) => {
+    const primeTime = ['15:30', '16:00'];
+    const isOnlyPrimeLeft = [...allGreenSlots].every((slot) => primeTime.includes(slot.innerHTML));
 
-if (allGreenSlots.length) {
+    if (isOnlyPrimeLeft) {
+      if (clickPrimeIfOnlyPrimeIsLeft) {
+        catchRandomDateSlot();
+      }
+      return;
+    }
+
+    const clickNoPrimeDate = () => {
+      const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
+      const slotToClick = allGreenSlots[randomSlotNumber];
+      if (primeTime.includes(slotToClick.innerHTML)) {
+        return clickNoPrimeDate();
+      }
+      slotToClick.click();
+    }
+
+    clickNoPrimeDate();
+  }
+
+  const catchPrimeOrRandomDateSlot = () => {
+    const first = '16:00';
+    const second = '15:30';
+
+    let primeSlotIndex = [...allGreenSlots].findIndex((slot) => slot.innerHTML === first);
+
+    if (primeSlotIndex === -1) {
+      primeSlotIndex = [...allGreenSlots].findIndex((slot) => slot.innerHTML === second);
+    }
+
+    if (primeSlotIndex >= 0) {
+      allGreenSlots[primeSlotIndex].click();
+    } else {
+      const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
+      const slotToClick = allGreenSlots[randomSlotNumber];
+      slotToClick.click();
+    }
+  }
+
+  // -----------------------------------------------------------------------
+
   catchRandomDateSlot(); // for ALL slots
   // catchNoPrimeDateSlot(); // for NO PRIME slots
   // catchNoPrimeDateSlot(true); // for NO PRIME or if only prime - ANY slot
+  // catchPrimeOrRandomDateSlot(); // for PRIME or any
 
-  makeAttention();
+  // -----------------------------------------------------------------------
+
+  const makeAttention = () => {
+    audio.play();
+    setTimeout(() => makeAttention(), 800);
+  };
+
+  makeAttention()
 }
+
+if (allGreenSlots.length) {
+  DATES();
+}
+
+const login = () => {
+  const loginBtn = document.querySelector('input[value="Log in"]');
+
+  if (loginBtn) {
+    const emailInput = document.getElementById('email');
+    const pwdInput = document.getElementById('pwd');
+
+    const newLoginBtn = document.createElement("button");
+    newLoginBtn.innerHTML = "ЛОГИН";
+    newLoginBtn.classList.add("my-login-btn");
+
+    newLoginBtn.addEventListener("click", () => {
+      const LOGIN = "login@gmail.com";
+      const PASSWORD = "my password";
+
+      emailInput.value = LOGIN;
+      pwdInput.value = PASSWORD;
+
+      loginBtn.click();
+    });
+
+    const body = document.getElementsByTagName("body")[0];
+    body.appendChild(newLoginBtn);
+  }
+}
+login();
