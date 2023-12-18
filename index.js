05ad19ed21c8f53873aaf307a2ec426bd142b02b
Yana <jana.ru.sidorova@yandex.ru>
2023-12-19 00:44:22 +0300

    feat: add register

--- index.js
+++ index.js
@@ -1,133 +1,168 @@
 setTimeout(() => {
-  window.scroll(0, 0);
-  window.scrollBy(0, 130);
+    window.scroll(0, 0);
+    window.scrollBy(0, 130);
 }, 900);
 
 const CLASS_TO_FIND = 'a.dispo';
 const allGreenSlots = document.querySelectorAll(CLASS_TO_FIND);
 
 const DATES = () => {
-  document.title = "!! ДАТЫ !!";
-  const audio = new Audio();
-  audio.src = 'http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a';
+    const audio = new Audio();
+    audio.src = 'http://commondatastorage.googleapis.com/codeskulptor-assets/week7-brrring.m4a';
 
-  function getRandomNumber(min, max) { // max excluded
-    return Math.floor(Math.random() * (max - min) + min);
-  }
-
-  const catchRandomDateSlot = () => {
-    const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
-    const slotToClick = allGreenSlots[randomSlotNumber];
-    slotToClick.click();
-  }
-
-  const catchNoPrimeDateSlot = (clickPrimeIfOnlyPrimeIsLeft = false) => {
-    const primeTime = ['15:30', '16:00'];
-    const isOnlyPrimeLeft = [...allGreenSlots].every((slot) => primeTime.includes(slot.innerHTML));
-
-    if (isOnlyPrimeLeft) {
-      if (clickPrimeIfOnlyPrimeIsLeft) {
-        catchRandomDateSlot();
-      }
-      return;
+    function getRandomNumber(min, max) { // max excluded
+        return Math.floor(Math.random() * (max - min) + min);
     }
 
-    const clickNoPrimeDate = () => {
-      const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
-      const slotToClick = allGreenSlots[randomSlotNumber];
-      if (primeTime.includes(slotToClick.innerHTML)) {
-        return clickNoPrimeDate();
-      }
-      slotToClick.click();
+    const catchRandomDateSlot = () => {
+        const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
+        const slotToClick = allGreenSlots[randomSlotNumber];
+        slotToClick.click();
     }
 
-    clickNoPrimeDate();
-  }
+    const catchNoPrimeDateSlot = (clickPrimeIfOnlyPrimeIsLeft = false) => {
+        const primeTime = ['15:30', '16:00'];
+        const isOnlyPrimeLeft = [...allGreenSlots].every((slot) => primeTime.includes(slot.innerHTML));
 
-  const catchPrimeOrRandomDateSlot = () => {
-    const first = '16:00';
-    const second = '15:30';
+        if (isOnlyPrimeLeft) {
+            if (clickPrimeIfOnlyPrimeIsLeft) {
+                catchRandomDateSlot();
+            }
+            return;
+        }
 
-    let primeSlotIndex = [...allGreenSlots].findIndex((slot) => slot.innerHTML === first);
+        const clickNoPrimeDate = () => {
+            const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
+            const slotToClick = allGreenSlots[randomSlotNumber];
+            if (primeTime.includes(slotToClick.innerHTML)) {
+                return clickNoPrimeDate();
+            }
+            slotToClick.click();
+        }
 
-    if (primeSlotIndex === -1) {
-      primeSlotIndex = [...allGreenSlots].findIndex((slot) => slot.innerHTML === second);
+        clickNoPrimeDate();
     }
 
-    if (primeSlotIndex >= 0) {
-      allGreenSlots[primeSlotIndex].click();
-    } else {
-      const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
-      const slotToClick = allGreenSlots[randomSlotNumber];
-      slotToClick.click();
+    const catchPrimeOrRandomDateSlot = () => {
+        const first = '16:00';
+        const second = '15:30';
+
+        let primeSlotIndex = [...allGreenSlots].findIndex((slot) => slot.innerHTML === first);
+
+        if (primeSlotIndex === -1) {
+            primeSlotIndex = [...allGreenSlots].findIndex((slot) => slot.innerHTML === second);
+        }
+
+        if (primeSlotIndex >= 0) {
+            allGreenSlots[primeSlotIndex].click();
+        } else {
+            const randomSlotNumber = getRandomNumber(0, allGreenSlots.length);
+            const slotToClick = allGreenSlots[randomSlotNumber];
+            slotToClick.click();
+        }
     }
-  }
 
-  // -----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
 
-  catchRandomDateSlot(); // for ALL slots
-  // catchNoPrimeDateSlot(); // for NO PRIME slots
-  // catchNoPrimeDateSlot(true); // for NO PRIME or if only prime - ANY slot
-  // catchPrimeOrRandomDateSlot(); // for PRIME or any
+    catchRandomDateSlot(); // for ALL slots
+    // catchNoPrimeDateSlot(); // for NO PRIME slots
+    // catchNoPrimeDateSlot(true); // for NO PRIME or if only prime - ANY slot
+    // catchPrimeOrRandomDateSlot(); // for PRIME or any
 
-  // -----------------------------------------------------------------------
+    // -----------------------------------------------------------------------
 
-  const makeAttention = () => {
-    audio.play();
-    setTimeout(() => makeAttention(), 800);
-  };
+    const makeAttention = () => {
+        audio.play();
+        setTimeout(() => makeAttention(), 800);
+    };
 
-  makeAttention()
+    makeAttention()
 }
 
 if (allGreenSlots.length) {
-  sendToTG();
-  DATES();
+    sendToTG();
+    DATES();
 }
 
-const login = () => {
-  const loginBtn = document.querySelector('input[value="Log in"]');
+// =============== LOGIN ================
 
-  if (loginBtn) {
+const loginBtn = document.querySelector('input[value="Log in"]');
+const login = () => {
+    const people = [{
+        label: "Name",
+        login: "example@mail.com",
+        pass: "pas123",
+    }, {
+        label: "Name 2",
+        login: "example@mail.com",
+        pass: "pas123",
+    }];
+
     const emailInput = document.getElementById('email');
     const pwdInput = document.getElementById('pwd');
 
-    const newLoginBtn = document.createElement("button");
-    newLoginBtn.innerHTML = "ЛОГИН";
-    newLoginBtn.classList.add("my-login-btn");
+    let btnStartIndex = 1;
+    people.forEach((person) => {
+        const newLoginBtn = document.createElement("button");
+        newLoginBtn.innerHTML = person.label;
+        newLoginBtn.classList.add(`my-login-btn${btnStartIndex++}`);
 
-    newLoginBtn.addEventListener("click", () => {
-      const LOGIN = "login@gmail.com";
-      const PASSWORD = "my password";
+        newLoginBtn.addEventListener("click", () => {
+            emailInput.value = person.login;
+            pwdInput.value = person.pass;
 
-      emailInput.value = LOGIN;
-      pwdInput.value = PASSWORD;
+            loginBtn.click();
+            setTimeout(() => loginBtn.click(), 1000);
+        });
 
-      loginBtn.click();
+        body.appendChild(newLoginBtn);
     });
-
-    const body = document.getElementsByTagName("body")[0];
-    body.appendChild(newLoginBtn);
-  }
 }
-login();
+
+if (loginBtn) {
+    login();
+}
+
+// ============== REGISTER ================
+
+const userForRegister = null; // { email: '', pwd: '' };
+const registerBtn = document.querySelector('input[value="Create"]');
+const register = () => {
+    const emailInput = document.getElementById('u_email');
+    const email2Input = document.getElementById('u_email_confirm');
+    const passwordInput = document.getElementById('u_password');
+    const password2Input = document.getElementById('u_password_confirm');
+
+    emailInput.value = userForRegister?.email;
+    email2Input.value = userForRegister?.email;
+    passwordInput.value = userForRegister?.pwd;
+    password2Input.value = userForRegister?.pwd;
+
+    registerBtn.click();
+};
+
+if (userForRegister && registerBtn) {
+    register();
+}
+
+// ============ TG ==========
 
 function sendToTG() {
-  // ВСТАВИТЬ НИЖЕ 2 строчки -----
+    // ВСТАВИТЬ НИЖЕ 2 строчки -----
 
 
 
-  // ------
+    // ------
 
-  fetch(`https://api.telegram.org/bot${bot_token}/sendMessage`, {
-    method: "POST",
-    body: JSON.stringify({
-      chat_id,
-      parse_mode: "HTML",
-      text: "ДАТЫ ДАТЫ ДАТЫ",
-    }),
-    headers: {
-      "Content-type": "application/json; charset=UTF-8"
-    }
-  });
+    fetch(`https://api.telegram.org/bot${bot_token}/sendMessage`, {
+        method: "POST",
+        body: JSON.stringify({
+            chat_id,
+            parse_mode: "HTML",
+            text: `ДАТЫ ДАТЫ ДАТЫ [${allGreenSlots.length}] - ${[...allGreenSlots].map((slot) => slot.innerHTML).join(', ')}`,
+        }),
+        headers: {
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
 }
